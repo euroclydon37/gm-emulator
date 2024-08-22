a4ca01fb524f7f748e23c8125303ea8e7eb970e2
Jeremy <jeremydunn29@gmail.com>
2024-08-22 14:45:47 -0500

    add dice rolling

---
+++ src/commands/diceCommand.ts
@@ -0,0 +1,30 @@
+import prompts from "prompts";
+import { randomNumber, wrapOutput } from "../utils.js";
+import chalk from "chalk";
+
+const rollDiceCombo = (combo: string) => {
+  const dice = combo.split("d");
+  const numDice = parseInt(dice[0]);
+  const dieSize = parseInt(dice[1]);
+  return Array.from(
+    { length: numDice },
+    () => `d${dieSize}: ${randomNumber(1, dieSize)}`
+  );
+};
+
+export const DiceCommand = {
+  name: "Roll dice",
+  run: async () => {
+    const { dice_combo } = await prompts({
+      type: "text",
+      name: "dice_combo",
+      message: "What combo? (e.g. 1d20+1d6)",
+    });
+
+    const combos: string[] = dice_combo.split("+");
+
+    console.log(
+      wrapOutput(chalk.yellow(combos.map(rollDiceCombo).flat().join("\n")))
+    );
+  },
+};
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,10 +2,12 @@ import { OracleCommand } from "./oracle.js";
 import { ActionThemeCommand } from "./actionTheme.js";
 import { DescriptorFocusCommand } from "./descriptorFocus.js";
 import { ManageLogsCommand } from "./manageLogs.js";
+import { DiceCommand } from "./diceCommand.js";
 
 export const commands = {
   OracleCommand,
   ActionThemeCommand,
   DescriptorFocusCommand,
   ManageLogsCommand,
+  DiceCommand,
 };
